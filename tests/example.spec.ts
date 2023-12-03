32f73d442312fa2074c017292efd1ec6278499ac
your name here <nickolaswdreid@gmail.com>
2023-12-03 19:15:35 -0400

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -27,3 +27,11 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('Look up Sephora', async ({ page }) => {
+  await page.goto('https://amazon.ca');
+  await page.locator('#twotabsearchtextbox').fill('Sephora');
+  await page.keyboard.down("Enter");
+
+
+});
