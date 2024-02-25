0eda9fe7782490a181d78e323ad33d0c76e96201
your name here <nickolaswdreid@gmail.com>
2024-02-25 18:01:40 -0400

    .

--- k6.js
+++ k6.js
@@ -1,12 +1,26 @@
 import http from 'k6/http';
 
+import { Rate } from 'k6/metrics';
+import { check } from 'k6';
+
+const failRate = new Rate('failed_requests');
+
+export const options = {
+  thresholds: {
+    failed_requests: ['rate<=0'],
+    http_req_duration: ['p(95)<500'],
+  },
+  ext: {
+    loadimpact: {
+      projectID: 3542013,
+    },
+  },
+};
+
 export default function () {
-  http.get(`http://127.0.0.1/get`);
-  // http.get(`http://localhost/get/status/200`);
-  // const params = {
-  //   headers: {
-  //     'Accept': 'image/png'
-  //   },
-  // };
-  // http.get(`http://localhost/get/image`, params);
+  const result = http.get('https://test-api.k6.io');
+  check(result, {
+    'http response status code is 200': result.status === 200,
+  });
+  failRate.add(result.status !== 200);
 }
