0f0302ab1ac88506c54cfbfa5addbc5361e354d3
your name here <nickolaswdreid@gmail.com>
2024-02-25 17:18:44 -0400

    .

---
+++ k6.js
@@ -0,0 +1,27 @@
+import http from 'k6/http';
+
+export let options = {
+    scenarios: {
+      stress_test: {
+        executor: 'ramping-vus',
+        startVUs: 0,
+        stages: [
+          { duration: '2m', target: 100 }, // ramp up to 100 VUs in 2 minutes
+          { duration: '5m', target: 100 }, // stay at 100 VUs for 5 minutes
+          { duration: '2m', target: 0 },   // ramp down to 0 VUs in 2 minutes
+        ],
+        gracefulRampDown: '1m',
+      },
+    },
+  };
+
+export default function () {
+  http.get('/get');
+  http.get("/get/status/200");
+  const params = {
+    headers: {
+      'Accept': 'image/png'
+    },
+  };
+  http.get("/get/image", params);
+}
