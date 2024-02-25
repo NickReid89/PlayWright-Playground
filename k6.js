9c88f540323560d468a6c0208d6d2c77d63a32c0
your name here <nickolaswdreid@gmail.com>
2024-02-25 18:24:08 -0400

    .

--- k6.js
+++ k6.js
@@ -1,26 +1,27 @@
 import http from 'k6/http';
 
-import { Rate } from 'k6/metrics';
-import { check } from 'k6';
-
-const failRate = new Rate('failed_requests');
-
-export const options = {
-  thresholds: {
-    failed_requests: ['rate<=0'],
-    http_req_duration: ['p(95)<500'],
-  },
-  ext: {
-    loadimpact: {
-      projectID: 3542013,
+export let options = {
+  scenarios: {
+    stress_test: {
+      executor: 'ramping-vus',
+      startVUs: 0,
+      stages: [
+        { duration: '2m', target: 100 }, // ramp up to 100 VUs in 2 minutes
+        { duration: '5m', target: 100 }, // stay at 100 VUs for 5 minutes
+        { duration: '2m', target: 0 },   // ramp down to 0 VUs in 2 minutes
+      ],
+      gracefulRampDown: '1m',
     },
   },
 };
 
 export default function () {
-  const result = http.get('http://httpbin/get');
-  check(result, {
-    'http response status code is 200': result.status === 200,
-  });
-  failRate.add(result.status !== 200);
+http.get('http://httpbin/get');
+http.get("http://httpbin/get/status/200");
+const params = {
+  headers: {
+    'Accept': 'image/png'
+  },
+};
+http.get("http://httpbin/get/image", params);
 }
